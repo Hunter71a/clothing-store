ed094d573cbe11e97da056fcafa8ec31d2534d21
Carl Olson <colson2275@gmail.com>
2022-03-18 19:54:43 -0400

    add navegation component

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,17 +1,21 @@
 import { Fragment } from 'react';
 import { Outlet, Link } from 'react-router-dom';
 
+import { ReactComponent as Crownlogo } from '../../assets/crown.svg';
+import './navigation.styles.scss';
+
 const Navigation = () => {
   return (
     <Fragment>
       <div className='navigation'>
         <Link className='logo-container' to='/'>
-          <div>Logo</div>
+          <Crownlogo className='logo' />
         </Link>
-        <div> className='links-container'</div>
-        <Link className='nav-link' to='/shop'>
-          SHOP
-        </Link>
+        <div className='nav-links-container'>
+          <Link className='nav-link' to='/shop'>
+            SHOP
+          </Link>
+        </div>
       </div>
       <Outlet />
     </Fragment>
