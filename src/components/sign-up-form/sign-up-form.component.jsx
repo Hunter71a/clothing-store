1c56c8abbdf4248355aeef1dcbc5f9c03752b465
Carl Olson <colson9@student.cscc.edu>
2022-03-28 10:34:30 -0400

    add more on sign up form

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -2,7 +2,10 @@ const SignUpForm = () => {
   return (
     <div>
       <h1>Sign up with your email and password</h1>
-      <form></form>
+      <form>
+        <label></label>
+        <input required />
+      </form>
     </div>
   );
 };
