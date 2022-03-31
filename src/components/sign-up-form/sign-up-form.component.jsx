1ef329c4a6f54d36c703e67129187b1817556915
Carl Olson <colson9@student.cscc.edu>
2022-03-31 19:05:19 -0400

    add sign in component

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -78,43 +78,11 @@ const SignUpForm = () => {
           type="password"
           required
           onChange={handleChange}
-          name="displayName"
-          value={displayName}
-        />
-        <FormInput
-          label="Confirm Password"
-          type="password"
-          required
-          onChange={handleChange}
-          name="confirmPassword"
-          value={confirmPassword}
-        />
-        <label>Display Name</label>
-        <input
-          type="text"
-          required
-          onChange={handleChange}
-          name="displayName"
-          value={displayName}
-        />
-        <label>Email</label>
-        <input
-          type="email"
-          required
-          onChange={handleChange}
-          name="email"
-          value={email}
-        />
-        <label>Password</label>
-        <input
-          type="password"
-          required
-          onChange={handleChange}
           name="password"
           value={password}
         />
-        <label>Confirm Password</label>
-        <input
+        <FormInput
+          label="Confirm Password"
           type="password"
           required
           onChange={handleChange}
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,21 +1,21 @@
-import { Fragment } from 'react';
-import { Outlet, Link } from 'react-router-dom';
+import { Fragment } from "react";
+import { Outlet, Link } from "react-router-dom";
 
-import { ReactComponent as Crownlogo } from '../../assets/crown.svg';
-import './navigation.styles.scss';
+import { ReactComponent as Crownlogo } from "../../assets/crown.svg";
+import "./navigation.styles.scss";
 
 const Navigation = () => {
   return (
     <Fragment>
-      <div className='navigation'>
-        <Link className='logo-container' to='/'>
-          <Crownlogo className='logo' />
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <Crownlogo className="logo" />
         </Link>
-        <div className='nav-links-container'>
-          <Link className='nav-link' to='/shop'>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
             SHOP
           </Link>
-          <Link className='nav-link' to='/sign-in'>
+          <Link className="nav-link" to="/auth">
             SIGN IN
           </Link>
         </div>
