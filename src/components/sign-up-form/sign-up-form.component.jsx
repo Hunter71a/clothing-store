5de2f911ee9cf24a7a2f3a85f491e1b098b2382c
Carl Olson <colson9@student.cscc.edu>
2022-04-01 11:07:04 -0400

    add setContext to sign up component

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -23,6 +23,8 @@ const SignUpForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { displayName, email, password, confirmPassword } = formFields;
 
+  const { setCurrentUser } = useContext(UserContext);
+
   const resetFormFields = () => {
     setFormFields(defaultFormFields);
   };
@@ -40,6 +42,8 @@ const SignUpForm = () => {
         password
       );
       await createUserDocumentFromAuth(user, { displayName });
+      setCurrentUser(user);
+
       resetFormFields();
     } catch (error) {
       if (error.code === "auth/email-already-in-use") {
