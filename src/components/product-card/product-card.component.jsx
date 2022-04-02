375a750b9fd1a05c6d98999dac1f0c078be8dd73
Carl Olson <colson9@student.cscc.edu>
2022-04-02 14:10:50 -0400

    make more mobile ready with flexbox

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,9 +1,17 @@
 import './product-card.styles.scss';
 
-const ProductCard = () => {
+import Button from '../button/button.component';
+
+const ProductCard = ({ product }) => {
+  const { name, price, imageUrl } = product;
   return (
     <div className='product-card-container'>
-      <img></img>
+      <img src={imageUrl} alt={name} />
+      <div className='footer'>
+        <span className='name'>{name}</span>
+        <span className='price'>{price}</span>
+      </div>
+      <Button buttonType='inverted'>Add to cart</Button>
     </div>
   );
 };
