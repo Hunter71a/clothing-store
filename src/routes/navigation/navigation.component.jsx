4a55976a6ffb5696344020fcb4c41fa2b75a37aa
Carl Olson <colson9@student.cscc.edu>
2022-04-02 16:01:45 -0400

    add shapping bag icon to nav bar

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,12 +1,14 @@
-import { Fragment, useContext } from "react";
-import { Outlet, Link } from "react-router-dom";
+import { Fragment, useContext } from 'react';
+import { Outlet, Link } from 'react-router-dom';
 
-import { ReactComponent as Crownlogo } from "../../assets/crown.svg";
-import { UserContext } from "../../contexts/user.context";
+import CartIcon from '../../components/cart-icon/cart-icon.component';
 
-import { signOutUser } from "../../utils/firebase/firebase.utils";
+import { ReactComponent as Crownlogo } from '../../assets/crown.svg';
+import { UserContext } from '../../contexts/user.context';
 
-import "./navigation.styles.scss";
+import { signOutUser } from '../../utils/firebase/firebase.utils';
+
+import './navigation.styles.scss';
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
@@ -15,23 +17,24 @@ const Navigation = () => {
 
   return (
     <Fragment>
-      <div className="navigation">
-        <Link className="logo-container" to="/">
-          <Crownlogo className="logo" />
+      <div className='navigation'>
+        <Link className='logo-container' to='/'>
+          <Crownlogo className='logo' />
         </Link>
-        <div className="nav-links-container">
-          <Link className="nav-link" to="/shop">
+        <div className='nav-links-container'>
+          <Link className='nav-link' to='/shop'>
             SHOP
           </Link>
           {currentUser ? (
-            <span className="nav-link" onClick={signOutUser}>
+            <span className='nav-link' onClick={signOutUser}>
               SIGN OUT
             </span>
           ) : (
-            <Link className="nav-link" to="/auth">
+            <Link className='nav-link' to='/auth'>
               SIGN IN
             </Link>
           )}
+          <CartIcon />
         </div>
       </div>
       <Outlet />
